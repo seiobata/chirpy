b89483274196a9bb414dcb89ee04db2bf9b246ec
seiobata <s.bautista18@yahoo.com>
2025-07-25 09:39:35 -0500

    create main and add basic server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/seiobata/chirpy
+
+go 1.24.5
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	log.Println("Starting server on port 8080...")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
