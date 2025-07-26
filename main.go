6f99ff7dfe98c456a83f9d156e0629ed6a0490f6
seiobata <s.bautista18@yahoo.com>
2025-07-26 08:14:34 -0500

    add fileserver handler

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
+const (
+	rootPath = "."
+	port     = "8080"
+)
+
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(rootPath)))
+
 	server := http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	log.Println("Starting server on port 8080...")
+	log.Printf("Starting server on port %s...", port)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
