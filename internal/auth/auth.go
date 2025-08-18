2ff567434ed50e54ed3b8fe25f1edd2683e05f84
seiobata <s.bautista18@yahoo.com>
2025-08-18 07:56:23 -0500

    add MakeRefreshToken to auth package

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"errors"
 	"net/http"
 	"strings"
@@ -27,4 +29,10 @@ func GetBearerToken(headers http.Header) (string, error) {
 		return "", errors.New("token not found")
 	}
 	return strings.TrimSpace(token), nil
+}
+
+func MakeRefreshToken() (string, error) {
+	token := make([]byte, 32)
+	rand.Read(token)
+	return hex.EncodeToString(token), nil
 }
